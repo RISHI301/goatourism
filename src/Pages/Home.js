0ebe28ae054e9d1efcec3f54e390ddf5103b17fd
RISHI301 <rishijuneja27@gmail.com>
2021-09-14 20:41:25 +0530

    deleted the unnecessary components

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <React.Fragment>
         <Container fixed>
-          <CenteredGrid/>
+          
         </Container>
     </React.Fragment>
   );
